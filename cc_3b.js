6f2e345f6a452edecd568be20a14f7e02eedbe31
Laura <lauracastanovilla@usf.edu>
2025-09-10 17:53:11 -0400

    added inventory items and console.log

--- cc_3b.js
+++ cc_3b.js
@@ -1,2 +1,28 @@
 // Coding Challenge 03b
 
+let inventory = [{
+    sku: "SKU-004",
+    name: "iPhone 17",
+    price: 799.99,
+    stock: 1300,
+},
+{
+    sku: "SKU-007",
+    name: "iPad Air",
+    price: 850.99,
+    stock: 2000,
+},
+{
+    sku: "SKU-112",
+    name: "Apple Watch SE",
+    price: 399.99,
+    stock: 300,
+},
+{
+    sku: "SKU-110",
+    name: "Airpods Pro",
+    price: 290.99,
+    stock: 500,
+}]
+
+inventory.forEach(obj =>console.log(`SKU: ${obj.sku} |`, `Name: ${obj.name} |`, `Price: ${obj.price} |`, `Stock: ${obj.stock}`));
